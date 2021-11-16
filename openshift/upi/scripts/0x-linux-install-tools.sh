09ee99ef9efbd591c862f0e79d2352fdb12aee46
Balkrishna Pandey <bpandey@redhat.com>
2021-11-16 12:37:27 -0700

    initial commit, create ocp on osp using UPI
    Signed-off-by: Balkrishna Pandey <bpandey@redhat.com>

---
+++ openshift/upi/scripts/0x-linux-install-tools.sh
@@ -0,0 +1,9 @@
+# openshift-install
+curl -LO curl -LO https://mirror.openshift.com/pub/openshift-v4/clients/ocp/4.9.7/openshift-install-linux-4.9.7.tar.gz
+tar -zxvf openshift-install-linux-4.9.7.tar.gz
+rm -rf openshift-install-linux-4.9.7.tar.gz
+
+#oc, kubectl
+curl -LO curl -LO https://mirror.openshift.com/pub/openshift-v4/clients/ocp/4.9.7/openshift-client-linux-4.9.7.tar.gz
+tar -zxvf openshift-client-linux-4.9.7.tar.gz
+rm -rf openshift-client-linux-4.9.7.tar.gz
---
+++ openshift/upi/scripts/0x-mac-install-tools.sh
@@ -0,0 +1,10 @@
+# openshift-install
+curl -LO https://mirror.openshift.com/pub/openshift-v4/clients/ocp/4.9.7/openshift-install-mac-4.9.7.tar.gz
+tar -zxvf openshift-install-mac-4.9.7.tar.gz
+rm -rf openshift-install-mac-4.9.7.tar.gz
+
+
+#oc, kubectl
+curl -LO https://mirror.openshift.com/pub/openshift-v4/clients/ocp/4.9.7/openshift-client-mac-4.9.7.tar.gz
+tar -zxvf openshift-client-mac-4.9.7.tar.gz
+rm -rf openshift-client-mac-4.9.7.tar.gz
